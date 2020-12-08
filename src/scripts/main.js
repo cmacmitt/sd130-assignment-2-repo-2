d4a73b808e86a21d8c46ead56dc523f93bc85b6b
Joao Felipe - Johnny <joaobarbo@student.mitt.ca>
2020-12-08 14:53:03 -0300

    add code to main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,3 +3,19 @@ const addName = attendanceContainerEle.querySelector('button');
 const nameList = attendanceContainerEle.querySelector('ul');
 
 // === ADD SCRIPT FOR ADDING NAMES HERE === 
+
+let names = ['Johnny', 'Creston', 'Chet'];
+
+addName.addEventListener('click', event => {
+  for (let i = 0; i < names.length; i++) {
+    const addLi = document.createElement('LI');
+    const classmatesNames = document.createTextNode(names[i]);
+
+    addLi.appendChild(classmatesNames);
+    nameList.appendChild(addLi);
+
+    event.preventDefault();
+  }
+})
+
+attendanceContainerEle.body.appendChild(nameList);
